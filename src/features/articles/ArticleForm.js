8f964ebbaee09824cd5e842e7a51f18e92f6f14d
sky1045 <ghdwlgud4545@gmail.com>
2021-08-18 22:39:20 +0900

    add markdown form

--- src/features/articles/ArticleForm.js
+++ src/features/articles/ArticleForm.js
@@ -1,31 +1,59 @@
 import React from 'react'
-import { Input, makeStyles, TextField } from '@material-ui/core'
+import { 
+    Input,
+    makeStyles,
+    TextField,
+    Button,
+} from '@material-ui/core'
+import MDEditor from '@uiw/react-md-editor';
 
 const useStyles = makeStyles(theme => ({
     text: {
-        color: "black"
+        color: "black",
+        fontSize: '36px',
     },
     spacer: theme.mixins.toolbar
 }))
 
 const ArticleForm = (props) => {
     const classes = useStyles()
+    const [value, setValue] = React.useState("## MarkDown으로 작성해주세요");
+
+    const handleSubmit = (event) => {
+        console.log(event)
+        event.preventDefault()
+    }
     return (
-        <form noValidate >
-            <div>article form page</div>
+        <form noValidate onSubmit={handleSubmit}>
             <Input placeholder="제목"
                 label="제목"
                 inputProps={{
                     className: classes.text
                 }}
+                autoFocus
+                required
+                fullWidth
             />
-            <div className={classes.spacer} />
+            <MDEditor
+                value={value}
+                onChange={setValue}
+            />
+            {/* <MDEditor.Markdown source={value} /> */}
             <TextField
                 multiline
                 inputProps={{
                     className: classes.text
                 }}
             />
+            <div className={classes.spacer} />
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.button}
+            >
+                글 쓰기
+            </Button>
         </form>
     )
 }
